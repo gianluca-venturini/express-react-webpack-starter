7ac1ddecb305e1a7484a1afe6504d94dfd2ae2d2
Gianluca Venturini <gianluca@stellarite.io>
2020-05-12 19:10:03 -0700

    Frontend and backend builds

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-# webpack-start
-A starter Webpack 4 configuration for basic projects.
+# Express React Webpack starter
+A starter Webpack 4 configuration for basic projects with Express and React.
 
 # Features
-- Build single page typescript
+- Build single page web apps with typescript.
+
+# Dependencies
+- Install `node >= 10.18.0`
+    - Use NVM (https://github.com/nvm-sh/nvm): `nvm install 10.18.0 && nvm use 10.18.0`
+    - Alternatively you can download and install it manually: https://nodejs.org/en/download/
+- Install `yarn ^1.10.1`
+    - Use brew (https://brew.sh/): `brew install yarn`
+    - Alternatively you can download and install it manually: https://classic.yarnpkg.com/en/docs/install
 
 # Usage
-- Run dev server with hot reload:  `npm run server-dev`
+- Install packages: `yarn install`
+- Build application: `npm run build`
+- Run application: `npm start`
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,101 @@
+const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.ts',
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"]
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: false,
-    port: 8000
-  },
-  plugins: [
-    new HtmlWebpackPlugin({template: './src/index.html'})
-  ],
-  module: {
-    rules: [
-      { test: /\.tsx?$/, loader: "ts-loader" }
-    ]
-  }
+const PROJECTS = (process.env.PROJECTS || '').split(',');
+const IS_WATCH = !!process.env.WATCH;
+
+const resolveConfig = {
+    symlinks: false,
+    extensions: ['.js', '.json', '.ts', '.tsx']
 };
+
+const includeDirs = ['src'].map(x => path.resolve(__dirname, x));
+
+const moduleConfig = (tscOptions = {}) => ({
+    rules: [
+        {
+            test: /\.(ts|tsx)$/,
+            include: includeDirs,
+            use: [
+                {
+                    loader: 'ts-loader',
+                    options: {
+                        compilerOptions: {
+                            ...tscOptions
+                        }
+                    }
+                }
+            ]
+        }
+    ]
+});
+
+/**
+ * Lets server side webpack bundles use sourcemaps
+ * See: https://decembersoft.com/posts/how-to-fix-your-server-side-typescript-call-stack-with-webpack-bannerplugin/
+ */
+const sourceMapBannerPlugin = new webpack.BannerPlugin({ banner: 'require("source-map-support").install();', raw: true, entryOnly: false });
+
+const htmlPlugin = new HtmlWebpackPlugin({template: './src/frontend/index.html'});
+
+const frontendBuild = (name, entryFile, outputFile) => ({
+    name: name,
+    entry: entryFile,
+    output: {
+        path: __dirname + '/dist/www',
+        filename: outputFile
+    },
+    module: moduleConfig({ target: 'ES6', module: 'ES6' }),
+    mode: 'development',
+    devtool: 'cheap-module-eval-source-map',
+    resolve: resolveConfig,
+    watch: IS_WATCH,
+    devServer: {
+        historyApiFallback: true, // replace 404 page with index.html for client-side routing
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                secure: false
+            }
+        }
+    },
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows to avoid bundling all of your
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
+    plugins: [sourceMapBannerPlugin, htmlPlugin]
+});
+
+const backendBuild = (name, entryFile, outputFile = 'server.js') => {
+    return {
+        name: name,
+        target: 'node',
+        entry: entryFile,
+        output: {
+            path: __dirname + '/dist',
+            filename: outputFile,
+            pathinfo: true
+        },
+        watch: IS_WATCH,
+        module: moduleConfig({ target: 'ES2017', module: 'ES6' }),
+        mode: 'development',
+        devtool: 'source-map',
+        resolve: resolveConfig,
+        plugins: [sourceMapBannerPlugin]
+    };
+}
+
+const availableProjects = {
+
+    frontend: frontendBuild('frontend', './src/frontend/main.ts', 'bundle.js'),
+
+    backend: backendBuild('backend', './src/backend/main.ts', 'server.js'),
+
+};
+
+module.exports = PROJECTS.map(p => availableProjects[p]);
